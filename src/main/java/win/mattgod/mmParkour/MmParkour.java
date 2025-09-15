92f85dc16c693bc484d862375068806494eab8b0
Matthew Godwin <matthew_bat@outlook.com>
2025-09-15 17:47:19 +0100

    first commit

---
+++ src/main/java/win/mattgod/mmParkour/MmParkour.java
@@ -0,0 +1,17 @@
+package win.mattgod.mmParkour;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MmParkour extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
